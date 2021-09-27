214b08c662435791735d8fede184579050deec20
frappe <frappe@ubuntu>
2021-09-27 06:20:14 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Manage Shipping
+
+Frappe
+
+#### License
+
+MIT
---
+++ erpnext_shipping/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Manage Shipping",
+			"color": "blue",
+			"icon": "icon-book",
+			"type": "module",
+			"label": _("Manage Shipping")
+		}
+	]
